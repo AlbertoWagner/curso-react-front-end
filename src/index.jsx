e9aa663dcac1c598d373ff411465d796afcd0ad3
AlbertoWagner <alberto.oliveira@ccc.ufcg.edu.br>
2019-05-29 20:53:12 -0300

    aplicando redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {applyMiddleware,createStore} from 'redux'
+import {Provider} from 'react-redux'
+import promise from  'redux-promise'
+
 import App from './main/app'
+import reducers from './main/reduce'
 
-
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers,devTools)
 const element = document.getElementById('app')
-ReactDOM.render(<App/>,element)
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+,element)
